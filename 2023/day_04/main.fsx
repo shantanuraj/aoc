b2da143b837781658189aa31ace7508cada74791
Shantanu Raj <s@sraj.me>
2023-12-04 13:49:07 +0100

    feat(day_04): Add part 2

--- 2023/day_04/main.fsx
+++ 2023/day_04/main.fsx
@@ -16,7 +16,37 @@ let getSum (line: string) =
 let solve (input: string) =
     input.Split([|'\n'|]) |> Array.map getSum |> Array.sum
 
+// Returns function that takes a line and updates the array
+let cardCount (copies: int[]) =
+    fun (i: int) ->
+        fun(line: string) ->
+            let line = line.[line.IndexOf(": ") + 2..]
+
+            let left = line.[..line.IndexOf("|") - 1] |> getTokens
+            let right = line.[line.IndexOf("|") + 2..] |> getTokens
+
+            // Count how many numbers on the right are also on the left
+            let count = right |> Array.filter (fun n -> left |> Array.exists (fun m -> m = n)) |> Array.length
+
+            // For the next `count` elements, increment the number of copies by current line's number of copies
+            for j in i + 1 .. i + count do
+                copies.[j] <- copies.[j] + copies.[i]
+
+            copies.[i]
+
+let solve2 (input: string) =
+    let lines = input.Split([|'\n'|])
+
+    // Initialize an array with the same length as the input, set all values to 1
+    let mutable copies = Array.create lines.Length 1
+
+    lines |> Array.mapi (cardCount copies) |> Array.sum
+
 let input_01 = System.IO.File.ReadAllText("input_01.txt")
-printfn "input_01.txt -- %d" (solve input_01)
 let input_02 = System.IO.File.ReadAllText("input_02.txt")
+
+printfn "input_01.txt -- %d" (solve input_01)
 printfn "input_02.txt -- %d" (solve input_02)
+
+printfn "input_01.txt -- %d" (solve2 input_01)
+printfn "input_02.txt -- %d" (solve2 input_02)
